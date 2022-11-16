44cb3dcc116f93fbfdc28d6b78138c9c490b2879
rubiovega <cursoredes@localhost.localdomain>
2022-11-16 12:53:28 +0100

    redirecciones y duplicaciones ficheros

---
+++ Practica2_2/ejer12.cpp
@@ -0,0 +1,15 @@
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
+
+int main(int argc, char **argv)
+{
+
+    int fd = open(argv[1],O_RDWR);
+    dup2(fd,1);
+    write (1, "Salida standar redirigida a fichero\n", sizeof(char)*36);
+
+    return 0;
+}
